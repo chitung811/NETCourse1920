6d5ccfebdc67e5ecf899c11bf460a3d7eaab34dc
Hien Luong <hyhien@gmail.com>
2019-09-21 09:13:21 +0700

    D03 - Sync/Async

---
+++ D03_SyncAsync/D03_SyncAsync/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using D03_SyncAsync.Models;
+
+namespace D03_SyncAsync.Controllers
+{
+    public class HomeController : Controller
+    {
+        // /Home/Sync
+        public IActionResult Sync()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Demo demo = new Demo();
+            demo.Test01();
+            demo.Test02();
+            demo.Test03();
+
+            sw.Stop();
+
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+
+        // /Home/Async
+        public async Task<IActionResult> Async()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Demo demo = new Demo();
+            var x = demo.Test01Async();
+            var y = demo.Test02Async();
+            var z = demo.Test03Async();
+            await x; await y; await z;
+
+            sw.Stop();
+
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ D03_SyncAsync/D03_SyncAsync/Models/Demo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace D03_SyncAsync.Models
+{
+    public class Demo
+    {
+        public int Test01()
+        {
+            Thread.Sleep(5000);
+            return new Random().Next();
+        }
+        public async Task<int> Test01Async()
+        {
+            await Task.Delay(5000);
+            return new Random().Next();
+        }
+
+        public double Test02()
+        {
+            Thread.Sleep(2000);
+            return new Random().NextDouble();
+        }
+
+        public async Task<double> Test02Async()
+        {
+            await Task.Delay(2000);
+            return new Random().NextDouble();
+        }
+
+        public void Test03()
+        {
+            Thread.Sleep(3000);
+        }
+
+        public async Task Test03Async()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}
