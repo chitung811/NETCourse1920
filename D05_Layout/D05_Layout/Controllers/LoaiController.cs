9e08f70e16aaaece2651b4ba0c5926d9ec32b58a
Hien <hyhien@gmail.com>
2019-10-12 11:12:09 +0700

    Finish

---
+++ D05_Layout/D05_Layout/Controllers/LoaiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using D05_Layout.Models;
+using D05_Layout.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D05_Layout.Controllers
+{
+    public class LoaiController : Controller
+    {
+        private readonly IRepoLoai _repoLoai;
+        public LoaiController(IRepoLoai repoLoai)
+        {
+            _repoLoai = repoLoai;
+        }
+        public IActionResult Index()
+        {
+            return View(_repoLoai.GetAll());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Loai lo)
+        {
+            _repoLoai.Add(lo);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- D05_Layout/D05_Layout/Startup.cs
+++ D05_Layout/D05_Layout/Startup.cs
@@ -35,7 +35,8 @@ namespace D05_Layout
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddTransient<IRepoLoai, MockRepoLoai>();
+            //services.AddTransient<IRepoLoai, MockRepoLoai>();
+            services.AddTransient<IRepoLoai, SqlServerLoaiRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
