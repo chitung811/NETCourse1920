a11f89faea125031f4a739e2482af78c22ac5f77
Hien Luong <hyhien@gmail.com>
2019-10-19 09:28:15 +0700

    CRUD

---
+++ D06_EFCoreCodeFirst/D06_EFCoreCodeFirst/Controllers/MonHocController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using D06_EFCoreCodeFirst.DataModels;
+
+namespace D06_EFCoreCodeFirst.Controllers
+{
+    public class MonHocController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public MonHocController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MonHoc
+        public async Task<IActionResult> Index()
+        {
+            var myDbContext = _context.MonHocs.Include(m => m.MonDieuKien);
+            return View(await myDbContext.ToListAsync());
+        }
+
+        // GET: MonHoc/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monHoc = await _context.MonHocs
+                .Include(m => m.MonDieuKien)
+                .FirstOrDefaultAsync(m => m.MaMh == id);
+            if (monHoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(monHoc);
+        }
+
+        // GET: MonHoc/Create
+        public IActionResult Create()
+        {
+            ViewData["MonDk"] = new SelectList(_context.MonHocs, "MaMh", "MaMh");
+            return View();
+        }
+
+        // POST: MonHoc/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaMh,TenMh,SoTc,MonDk")] MonHoc monHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(monHoc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MonDk"] = new SelectList(_context.MonHocs, "MaMh", "MaMh", monHoc.MonDk);
+            return View(monHoc);
+        }
+
+        // GET: MonHoc/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monHoc = await _context.MonHocs.FindAsync(id);
+            if (monHoc == null)
+            {
+                return NotFound();
+            }
+            ViewData["MonDk"] = new SelectList(_context.MonHocs, "MaMh", "MaMh", monHoc.MonDk);
+            return View(monHoc);
+        }
+
+        // POST: MonHoc/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaMh,TenMh,SoTc,MonDk")] MonHoc monHoc)
+        {
+            if (id != monHoc.MaMh)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(monHoc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MonHocExists(monHoc.MaMh))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MonDk"] = new SelectList(_context.MonHocs, "MaMh", "MaMh", monHoc.MonDk);
+            return View(monHoc);
+        }
+
+        // GET: MonHoc/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monHoc = await _context.MonHocs
+                .Include(m => m.MonDieuKien)
+                .FirstOrDefaultAsync(m => m.MaMh == id);
+            if (monHoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(monHoc);
+        }
+
+        // POST: MonHoc/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var monHoc = await _context.MonHocs.FindAsync(id);
+            _context.MonHocs.Remove(monHoc);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MonHocExists(string id)
+        {
+            return _context.MonHocs.Any(e => e.MaMh == id);
+        }
+    }
+}
