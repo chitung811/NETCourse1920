82a0d3aad349880bb52c159fe77fab61e7c0ff71
Hien Luong <hyhien@gmail.com>
2019-10-19 10:17:01 +0700

    D07 - Database First

---
+++ D07_EFCore_DBFirst/D07_EFCore_DBFirst/Models/CauHinh.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D07_EFCore_DBFirst.Models
+{
+    public partial class CauHinh
+    {
+        public int Id { get; set; }
+        public int? SoSanPham1Trang { get; set; }
+        public int? SoTrang { get; set; }
+        public int? SoNgayHetHanQc { get; set; }
+    }
+}
---
+++ D07_EFCore_DBFirst/D07_EFCore_DBFirst/Models/Loai.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D07_EFCore_DBFirst.Models
+{
+    public partial class Loai
+    {
+        public Loai()
+        {
+            HangHoa = new HashSet<HangHoa>();
+        }
+
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; }
+        public string MoTa { get; set; }
+        public string Hinh { get; set; }
+
+        public virtual ICollection<HangHoa> HangHoa { get; set; }
+    }
+}
