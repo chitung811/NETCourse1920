c2b77cc4a128c885bbdc16469b91c1b615b7b079
Hien Luong <hyhien@gmail.com>
2019-10-19 10:49:25 +0700

    Search products - part 1

---
+++ D07_EFCore_DBFirst/D07_EFCore_DBFirst/Controllers/HangHoaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using D07_EFCore_DBFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D07_EFCore_DBFirst.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly MyeStoreContext _context;
+        public HangHoaController(MyeStoreContext ctx)
+        {
+            _context = ctx;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string TuKhoa, double? GiaTu, double? GiaDen)
+        {
+            var dsHangHoa = _context.HangHoa.AsQueryable();
+
+            if(!string.IsNullOrEmpty(TuKhoa))
+            {
+                dsHangHoa = dsHangHoa.Where(p => p.TenHh.Contains(TuKhoa)).AsQueryable();
+            }
+
+            if(GiaTu.HasValue)
+            {
+                dsHangHoa = dsHangHoa.Where(p => p.DonGia >= GiaTu.Value).AsQueryable();
+            }
+
+            if (GiaDen.HasValue)
+            {
+                dsHangHoa = dsHangHoa.Where(p => p.DonGia <= GiaDen.Value).AsQueryable();
+            }
+            return View();
+        }
+    }
+}
---
+++ D07_EFCore_DBFirst/D07_EFCore_DBFirst/ViewModels/HangHoaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D07_EFCore_DBFirst.ViewModels
+{
+    public class HangHoaViewModel
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public double GiaBan { get; set; }
+        public string Hinh { get; set; }
+        public string Loai { get; set; }
+        public string NhaCungCap { get; set; }
+    }
+}
