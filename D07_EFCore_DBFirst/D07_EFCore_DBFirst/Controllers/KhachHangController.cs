07fb7f36a3311e8c7f8d753f9914bb3a6230d50e
Hien Luong <hyhien@gmail.com>
2019-11-02 10:07:07 +0700

    D09 - Demo session for Login

---
+++ D07_EFCore_DBFirst/D07_EFCore_DBFirst/Controllers/KhachHangController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using D07_EFCore_DBFirst.Models;
+using D07_EFCore_DBFirst.Helpers;
+using D07_EFCore_DBFirst.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D07_EFCore_DBFirst.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly MyeStoreContext _context;
+        private readonly IMapper _mapper;
+        public KhachHangController(MyeStoreContext ctx, IMapper mapper)
+        {
+            _context = ctx;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            var kh = _context.KhachHang.SingleOrDefault(p => p.MaKh == model.MaKh && p.MatKhau == model.MatKhau);
+            if(kh != null)
+            {
+                HttpContext.Session.Set("KhachHang", kh);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("KhachHang");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ D07_EFCore_DBFirst/D07_EFCore_DBFirst/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D07_EFCore_DBFirst.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [MaxLength(20)]
+        public string MaKh { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        [MaxLength(50)]
+        public string MatKhau { get; set; }
+    }
+}
