40395ad8e551513d5012f73560d3eda83f5e983c
Hien Luong <hyhien@gmail.com>
2019-11-16 08:41:11 +0700

    D11 - Add JwtAuthen in ConfigugeService

---
+++ WebAPI/WebAPI/Controllers/LoaisController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using D07_EFCore_DBFirst.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private readonly MyeStoreContext _context;
+
+        public LoaisController(MyeStoreContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/Loais/search/???
+        [HttpGet("search/{keyword}")]
+        public IActionResult SearchLoai(string keyword = null)
+        {
+            var data = _context.Loai.AsQueryable();
+            if(!string.IsNullOrEmpty(keyword))
+            {
+                data = data.Where(p => p.TenLoai.Contains(keyword));
+            }
+            return this.Ok(data);
+        }
+
+        // GET: api/Loais
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Loai>>> GetLoai()
+        {
+            return await _context.Loai.ToListAsync();
+        }
+
+        // GET: api/Loais/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Loai>> GetLoai(int id)
+        {
+            var loai = await _context.Loai.FindAsync(id);
+
+            if (loai == null)
+            {
+                return NotFound();
+            }
+
+            return loai;
+        }
+
+        // PUT: api/Loais/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLoai(int id, Loai loai)
+        {
+            if (id != loai.MaLoai)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(loai).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoaiExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Loais
+        [HttpPost]
+        public async Task<ActionResult<Loai>> PostLoai(Loai loai)
+        {
+            _context.Loai.Add(loai);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLoai", new { id = loai.MaLoai }, loai);
+        }
+
+        // DELETE: api/Loais/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Loai>> DeleteLoai(int id)
+        {
+            var loai = await _context.Loai.FindAsync(id);
+            if (loai == null)
+            {
+                return NotFound();
+            }
+
+            _context.Loai.Remove(loai);
+            await _context.SaveChangesAsync();
+
+            return loai;
+        }
+
+        private bool LoaiExists(int id)
+        {
+            return _context.Loai.Any(e => e.MaLoai == id);
+        }
+    }
+}
--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using D07_EFCore_DBFirst.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,6 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace D07_EFCore_DBFirst
 {
@@ -47,13 +50,34 @@ namespace D07_EFCore_DBFirst
                 p.IdleTimeout = TimeSpan.FromMinutes(1);
             });
 
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(opt =>
+            //services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            //    .AddCookie(opt =>
+            //    {
+            //        opt.LoginPath = "/KhachHang/Login";
+            //        opt.AccessDeniedPath = "/Home/AccessDenied";
+            //        opt.LogoutPath = "/KhachHang/Logout";
+            //    });
+
+            var keyInConfig = Configuration["AppSettings:PrivatedKey"];
+            var key = Encoding.Unicode.GetBytes(keyInConfig);
+
+            //Authentication using JWT
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
                 {
-                    opt.LoginPath = "/KhachHang/Login";
-                    opt.AccessDeniedPath = "/Home/AccessDenied";
-                    opt.LogoutPath = "/KhachHang/Logout";
-                });
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }//end ConfigureService
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
